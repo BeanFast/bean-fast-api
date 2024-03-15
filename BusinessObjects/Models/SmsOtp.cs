b5a0e7924b77cad9d2088a3417d1e1cd0ceeb3c4
thanh-dao <15thanh09@gmail.com>
2024-03-15 22:16:57 +0700

    update

---
+++ BusinessObjects/Models/SmsOtp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Models
+{
+    public class SmsOtp : BaseEntity
+    {
+        public DateTime CreateAt { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public string Value { get; set; }
+        public virtual User User { get; set; }
+    }
+}
--- BusinessObjects/Models/User.cs
+++ BusinessObjects/Models/User.cs
@@ -9,11 +9,16 @@
         public string Password { get; set; }
         public string? Email { get; set; }
         public string AvatarPath { get; set; }
+        public string? SmsOtp { get; set; }
+        public string? RefreshToken { get; set; }
+        
+
         public virtual Role? Role { get; set; }
         public virtual ICollection<Profile>? Profiles { get; set; }
         public virtual ICollection<Menu>? CreatedMenus { get; set; }
         public virtual ICollection<Menu>? UpdatedMenus { get; set; }
         public virtual ICollection<SessionDetail>? SessionDetails { get; set; }
         public virtual ICollection<Wallet>? Wallets { get; set; }
+        public virtual ICollection<SmsOtp>? SmsOtps { get; set; }
     }
 }
