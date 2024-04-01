4008683a2c998acb8a4e73e118b3fbafc959335a
thanh-dao <15thanh09@gmail.com>
2024-04-01 20:24:33 +0700

    feat: add game table

---
+++ BusinessObjects/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Models
+{
+    public class Game : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; }
+    }
+}
--- BusinessObjects/Models/Transaction.cs
+++ BusinessObjects/Models/Transaction.cs
@@ -4,11 +4,13 @@
     {
         public Guid? OrderId { get; set; }
         public Guid? ExchangeGiftId { get; set; }
+        public Guid? GameId { get; set; }
         public Guid WalletId { get; set; }
         public string Code { get; set; }
         public DateTime Time { get; set; }
         public double Value { get; set; }
         public virtual Order? Order { get; set; }
+        public virtual Game? Game { get; set; }
         public virtual ExchangeGift? ExchangeGift { get; set; }
         public virtual Wallet? Wallet { get; set; }
     }
